306504336db53a965862607212be4a5b8aeab31f
Kyeonga Lee <klee.mcintosh@gmail.com>
2019-08-03 17:49:12 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,13 @@
+$status = "'Thank You Mario! But Our Princess Is In Another Castle!'"
+
 def mario
-  phrase = 'It's-a me, Mario!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
   puts status
 end
+
+def link
+  puts 
